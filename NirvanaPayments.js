61d0e92d6e31d5901971172a7c819fce2cd4d014
amverma48@gmail.com <amverma48@gmail.com>
2021-03-31 13:00:23 +0200

    Removed some comments in NirvanaPayments.js and updated ReadMe

--- NirvanaPayments.js
+++ NirvanaPayments.js
@@ -163,40 +163,7 @@ async function f1(){
 ]);
 
 const signedPayment = EthCrypto.sign(customerIdentity.privateKey, paymentMessage);
-/*
-function toUTF8Array(str) {
-    let utf8 = [];
-    for (let i = 0; i < str.length; i++) {
-        let charcode = str.charCodeAt(i);
-        if (charcode < 0x80) utf8.push(charcode);
-        else if (charcode < 0x800) {
-            utf8.push(0xc0 | (charcode >> 6),
-                      0x80 | (charcode & 0x3f));
-        }
-        else if (charcode < 0xd800 || charcode >= 0xe000) {
-            utf8.push(0xe0 | (charcode >> 12),
-                      0x80 | ((charcode>>6) & 0x3f),
-                      0x80 | (charcode & 0x3f));
-        }
-        // surrogate pair
-        else {
-            i++;
-            // UTF-16 encodes 0x10000-0x10FFFF by
-            // subtracting 0x10000 and splitting the
-            // 20 bits of 0x0-0xFFFFF into two halves
-            charcode = 0x10000 + (((charcode & 0x3ff)<<10)
-                      | (str.charCodeAt(i) & 0x3ff));
-            utf8.push(0xf0 | (charcode >>18),
-                      0x80 | ((charcode>>12) & 0x3f),
-                      0x80 | ((charcode>>6) & 0x3f),
-                      0x80 | (charcode & 0x3f));
-        }
-    }
-    return utf8;
-}
-*/
-//const signatureBytes = toUTF8Array(signedPayment);
-const vrsSignedPayment = EthCrypto.vrs.fromString(signedPayment);
+//const vrsSignedPayment = EthCrypto.vrs.fromString(signedPayment);
 console.log(vrsSignedPayment);
 
 const claimingPayment = contractInstance.methods.claim_payment(paymentToBeSent, vrsSignedPayment.v, vrsSignedPayment.r, vrsSignedPayment.s, customerIdentity.address).encodeABI();
@@ -218,36 +185,6 @@ console.log(customerInformation);
 const merchantInformation = await contractInstance.methods.merchants(merchantIdentity2.address).call();
 console.log(merchantInformation);
 web3.eth.getBalance(merchantIdentity1.address).then(console.log);
-    //const signedDetails = EthCrypto.sign(ownerIdentity.privateKey, detailHash);
-
-    //const signature = EthCrypto.sign(ownerIdentity.privateKey, signHash);
-
-    //const vrs = EthCrypto.vrs.fromString(signature);
-
-    //const vrsDetails = EthCrypto.vrs.fromString(signedDetails);
-
-    //transaction for setting the required days
-    //const requiredDays = contractInstance.methods.setRequiredDays(2).encodeABI();
-    //const requiredTx = {
-    //    from:customerIdentity.address,
-    //    to:contractAddress,
-    //    nonce:0,
-    //    gasLimit:5000000,
-    //    gasPrice:500000000,
-     //   data: requiredDays,
-    //}
-
-    //const serializedRequiredTx = EthCrypto.signTransaction(requiredTx, customerIdentity.privateKey);
-
-    //const receiptRequired = await web3.eth.sendSignedTransaction(serializedRequiredTx);
-
-
-    //transaction for sending the encrypted details to the blockchain
-    
-    //console.dir(messa);
-
-    //Transaction to call rentCar() function. Requires a value of 5 ether to be executed
-   
 
 }
 f1();
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # NirvanaPayments
 
-Smart contract which enables customers to deposit their collateral and make failsafe payments to merchants. Each customer can make mutliple payments to any merchant. 
+Nirvana.sol = Smart contract which enables customers to deposit their collateral and make failsafe payments to merchants. Each customer can make mutliple payments to any merchant. 
+
+NirvanaPaymentChannel.sol = Customer's deposit a collateral. This collateral can be used to send encrypted payment promises to merchants which can only be decrypted if the customer double spends. We still need to add the conditional decryption part. For now all payment promises are instantly available to the merchant which can be used for claimPayment() function. 
