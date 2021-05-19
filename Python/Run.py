16dc323dce75e011080a5056437c231982dc7940
Mahdi Sedaghat <s.m.sedaghat@gmail.com>
2021-05-19 11:51:02 +0200

    Adding the new results

--- Python/Run.py
+++ Python/Run.py
@@ -145,7 +145,7 @@ def run_round_trip(n,d,M):
         start_bench(groupObj)
         (mpk, msk) = Nir.Setup()
         setup_time += end_bench(groupObj)
-    setup_time = setup_time * 10
+    setup_time = setup_time  
     result.append(setup_time)
     public_parameters_size = sum([len(x) for x in serializeDict(mpk, groupObj).values()]) 
     result.append(public_parameters_size)
@@ -156,7 +156,7 @@ def run_round_trip(n,d,M):
         start_bench(groupObj)
         (pk,sk) = Nir.Keygen(mpk, msk, Merchants)
         Key_Gen_time += end_bench(groupObj)
-    Key_Gen_time = Key_Gen_time * 10
+    Key_Gen_time = Key_Gen_time  
     public_key_size = sum([len(x) for x in serializeDict(pk, groupObj).values()]) 
     secret_key_size = sum([len(x) for x in serializeDict(sk, groupObj).values()]) 
     secret_key_size = secret_key_size /10
@@ -172,7 +172,7 @@ def run_round_trip(n,d,M):
         start_bench(groupObj)
         (Col) = Nir.Registeration(mpk, msk, n)
         Registeration_time += end_bench(groupObj)
-    Registeration_time = Registeration_time * 10
+    Registeration_time = Registeration_time  
     Collateral_size = sum([len(x) for x in serializeDict(Col, groupObj).values()]) 
     Collateral_size = Collateral_size /10
     result.append(Registeration_time)
@@ -186,9 +186,9 @@ def run_round_trip(n,d,M):
         start_bench(groupObj)
         (ct1, Rand1,proof1,proof2,proof3,proof4) = Nir.Spending(mpk, Col, pk, time, d, N, IDsk, ID)
         Spending_time += end_bench(groupObj)
-    Spending_time = Spending_time * 10
+    Spending_time = Spending_time  
     result.append(Spending_time)
-    Ciphertext_size = sum([len(x) for x in serializeDict(ct1, groupObj).values()]) + sum([len(x) for x in serializeDict(Rand1, groupObj).values()]) 
+    Ciphertext_size = sum([len(x) for x in serializeDict(ct1, groupObj).values()]) + sum([len(x) for x in serializeDict(Rand1, groupObj).values()]) + sum([len(x) for x in serializeDict(proof1, groupObj).values()]) + sum([len(x) for x in serializeDict(proof2, groupObj).values()]) + sum([len(x) for x in serializeDict(proof3, groupObj).values()]) + sum([len(x) for x in serializeDict(proof4, groupObj).values()]) 
     result.append(Ciphertext_size)
     (ct2, Rand2,p1,p2,p3,p4) = Nir.Spending(mpk, Col, pk, time, d, N+1,IDsk,ID)
 
@@ -200,7 +200,7 @@ def run_round_trip(n,d,M):
         out = Nir.Verification(mpk, pk, Rand1, ct1, proof1, proof2, proof3, proof4 , d, Ledger, time, N)
         print(out)
         Verification_time += end_bench(groupObj)
-    Verification_time = Verification_time * 10
+    Verification_time = Verification_time  
     result.append(Verification_time) 
     #(out2)= Nir.Verification(mpk,ct2,Rand2)
 
@@ -211,7 +211,7 @@ def run_round_trip(n,d,M):
         start_bench(groupObj)
         (out)= Nir.Decryption(mpk, ct1, 1, ct2, 2)
         Decryption_time += end_bench(groupObj)
-    Decryption_time = Decryption_time * 10
+    Decryption_time = Decryption_time  
     result.append(Decryption_time)
 
     return result
@@ -220,7 +220,7 @@ book=Workbook()
 data=book.active
 title=["n","d","M","setup_time","public_parameters_size", "Key_Gen_time","public_key_size","secret_key_size","Registeration_time","Collateral_size","Spending_time","Ciphertext_size","Verification_time","Decryption_time"]
 data.append(title)
-for n in range(2,3):
+for n in range(1,30):
     data.append(run_round_trip(n,n,50*n))
     print(n)
 book.save("Result1.xlsx")
