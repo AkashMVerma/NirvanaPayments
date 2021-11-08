9065fc8b152abe6c548bfccdf3fcba193c0621b4
Mahdi Sedaghat <41582368+Mahdi171@users.noreply.github.com>
2021-11-08 10:58:24 +0330

    Some changes to Spending phase

--- Python/Nirvana.py
+++ Python/Nirvana.py
@@ -10,6 +10,7 @@ from openpyxl import Workbook
 from PoK import PoK
 from TSPS import TSPS
 from BLS import BLS01
+from secretshare import SecretShare as SSS
 
 '''
 mpk_t = { 'g':G1, 'h':G2, 'pp': G2, 'e_gh':GT, 'e_Xh':GT, 'vk':G2 , 'X': G1 }
@@ -86,66 +87,24 @@ class Nirvana():
         certprime=TSPS.Randomize(cert)
         return (Col,certprime)
     
-    def AuCreate(mpk,sgk,kprime,co,k):
-        cert={}
-        for i in range(co):
-            sigma1=TSPS.par_sign1(mpk,kprime,k)
-            sigma=TSPS.par_sign2(sigma1,sgk,k)
-            sigmaR=TSPS.reconst(sigma,k)
-            cert[i]=sigmaR
-        return cert
+    def AuCreate(mpk,sgk,kprime,Col,k):
+        sigma1=TSPS.par_sign1(mpk,kprime,k)
+        sigma=TSPS.par_sign2(sigma1,sgk,k)
+        sigmaR=TSPS.reconst(sigma,k)
+        Col['cert']=sigmaR
+        return Col
 
-    def Spending(self, mpk, Col, pk, time, d ,N, IDsk,ID):
-        SAgg=1; TAgg=1; PRFkey=0; R=[]; X=[]; y2=1
-        if len(Col['PRFkey']) >= d:
-            for i in range(d):
-                SAgg *= Col['S'][i]
-                TAgg *= Col['T'][i]
-                PRFkey += Col['PRFkey'][i]
-                R.append(mpk['e_gh'] ** (1/(Col['PRFkey'][i]+time)))
-                X.append(Col['PRFkey'][i])
-                y2 *= R[i] ** X[i]
-                A = pair(mpk['g'],mpk['vk']) ** PRFkey
-            tprime = group.random(ZR)
-            Rprime = Col['R'] ** (1/tprime)
-            Sprime = SAgg ** tprime
-            Tprime = (TAgg ** (tprime**2))* (Col['W']**(d*tprime*(1-tprime)))
-            Wprime = Col['W'] ** (1/tprime)
-            r = mpk['g'] ** (1/(PRFkey+time))
-            C = ID * (pair(r, mpk['pp']))
-            C1 = pair(r, pk['pk'][N])
-            (proof1) = PoK.prover3(mpk['g'],A,PRFkey,mpk['vk']) #Proof of SPS
-            (proof2) = PoK.prover4(y2,X,R) # Proof of Aggeragetd collatorals
-            (proof3) = PoK.prover3(r,C1**PRFkey,PRFkey,pk['pk'][N]) #Proof of ciphertext C1
-            (proof4) = PoK.prover2(C,mpk['e_gh'],((C/ID)**PRFkey)*(mpk['e_gh']**(-time*IDsk)),PRFkey,(-time*IDsk)) #Proof of ciphertext C0
-            Rand = { 'Rprime':Rprime, 'Sprime':Sprime, 'Tprime':Tprime, 'Wprime':Wprime }
-            ct = { 'C': C, 'C1': C1, 'R':R }
-            return (ct, Rand, proof1, proof2, proof3, proof4)
-        else:
-            return (print("You don't have enough money in your account"), None)
+    def Spending(self, mpk, Col, pk, time,N,ID,cert):
+        r = mpk['g'] ** (1/(Col['PRFkey']+time))
+        R = pair(r,mpk['h'])
+        C = ID * (pair(r, mpk['pp']))
+        C1 = pair(r, pk['pk'][N])
+        certprime=TSPS.Randomize(cert)
+        ct = { 'C': C, 'C1': C1, 'R':R }
+        return (ct, certprime)
 
 
-
-    def PPSpending(self, mpk, Col, pk, time, d ,N, IDsk, ID, PRFkey, X, A):
-        R=[]; y2=1
-        if len(Col['PRFkey']) >= d:
-            for i in range(d):
-                R.append(mpk['e_gh'] ** (1/(X[i]+time)))
-                y2 *= R[i] ** X[i]
-            r = mpk['g'] ** (1/(PRFkey+time))
-            C = ID * (pair(r, mpk['pp']))
-            C1 = pair(r, pk['pk'][N])
-            u = mpk['e_gh'] ** (PRFkey * IDsk)
-            (proof1) = PoK.prover3(mpk['g'],A,PRFkey,mpk['vk']) #Proof of SPS
-            (proof2) = PoK.prover4(y2,X,R) # Proof of Aggeragetd collatorals
-            (proof3) = PoK.prover3(r,C1**PRFkey,PRFkey,pk['pk'][N]) #Proof of ciphertext C1
-            (proof4) = PoK.prover2(C,mpk['e_gh'],((C/ID)**PRFkey)*(mpk['e_gh']**(-time*IDsk)),PRFkey,(-time*IDsk)) #Proof of ciphertext C0
-            ct = { 'C': C, 'C1': C1, 'R':R, 'u':u }
-            return (ct, proof1, proof2, proof3, proof4)
-        else:
-            return (print("You don't have enough money in your account"), None)
-
-    def Verification(self, mpk, pk, Rand, L1, L2, ct, proof1, proof2, proof3, proof4, d, Ledger, time, N):
+    def Verification(self, mpk, pk, Rand, L1, L2, ct, certprime, d, Ledger, time, N):
         LHS=1
         for i in range(len(ct['R'])):
             LHS *= (mpk['e_gh'] * ct['R'][i] ** (-time)) 
